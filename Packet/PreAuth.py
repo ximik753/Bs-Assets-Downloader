2d72144a90108bd16a0969a08d5321c8eee6e5af
Ximik <34097384+Sc-Ximik@users.noreply.github.com>
2019-06-15 20:17:46 +0300

    Add files via upload

---
+++ Packet/PreAuth.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from Packet.Writer import Writer
+
+
+class PreAuth(Writer):
+
+    def __init__(self):
+        self.Id = 10100
+
+    def process(self):
+        self.putInt(2)
+        self.putInt(6)
+        self.putInt(18)
+        self.putInt(0)
+        self.putInt(106)
+        self.putString('')
+        self.putInt(2)
+        self.putInt(2)
